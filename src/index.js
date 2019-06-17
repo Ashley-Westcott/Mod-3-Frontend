4f292126868219c2b7f397bb1e7f5e02931824e6
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-06-17 15:09:52 -0400

    ashley-frontend-commit-1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//run npm install -g json-server
+// run json-server --watch db.json
+
+// /* src/index.js */
+// document.addEventListener('DOMContentLoaded', () => {
+//   alert('LOADED');
+// });
