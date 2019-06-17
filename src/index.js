0ca23bb9b4317075fc56b6a94db21dee7a90e2f4
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-06-17 18:04:20 -0400

    ashley end of day

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 //run npm install -g json-server
 // run json-server --watch db.json
 
-// /* src/index.js */
-// document.addEventListener('DOMContentLoaded', () => {
-//   alert('LOADED');
-// });
+/* src/index.js */
+document.addEventListener('DOMContentLoaded', () => {
+  const endPoint = 'http://localhost:3000/api/v1/users';
+  fetch(endPoint)
+    .then(res => res.json())
+    .then(json => console.log(json));
+
+    
+});
