4c1125e1c0b8f5400cc5b9613b1bfd17b3b36cb5
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-06-20 12:27:31 -0400

    ashley changes thursday am

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@
 /* src/index.js */
 // document.addEventListener('DOMContentLoaded', () => {
 
-// let cupidInterval
-  // let heartInterval
-  
+  let heartInterval
+
   const endPoint = 'http://localhost:3000/api/v1/users';
   fetch(endPoint)
     .then(res => res.json())
@@ -16,15 +15,16 @@
   const cupid = document.getElementById('cupid')
   let  SCORE = 0
 
-
+  //Initialize game w/ cupid in place
   function init(){
          cupid.style.position='relative';
-         cupid.style.left='180px';
+         cupid.style.left='280px';
          cupid.style.top='460px';
      }
 
   init()
 
+  //Cupid movement left & right
   function moveLeft(){
     if (cupid.offsetLeft > -5) {
     cupid.style.left=parseInt(cupid.style.left)-15 + 'px';
@@ -36,8 +36,6 @@
     cupid.style.left=parseInt(cupid.offsetLeft)+15 + 'px';
   }
 }
-
-
 
   function letCupidMove(event) {
     if (event.key === "ArrowLeft") {
@@ -52,22 +50,16 @@
     }
   }
 
-  //  event listener for letcupidmove
+  //Event listener for letCupidMove
   window.addEventListener("keydown", letCupidMove)
 
-
-
- 
-
-
-    const FALLING_HEARTS = [];
+  //Creating / moving hearts down
+    let FALLING_HEARTS = [];
     const gameContainer = document.getElementById("video-game-area")
-    let heartInterval
+
 
     function createHeart() {
       let heart = document.createElement('IMG');
-      // let heartSpriteImg = heart[Math.floor(Math.random()*heart.length)]
-      // heart.scr = heartSpriteImg
       heart.id = 'heart';
       heart.setAttribute('src', 'images/heart.png');
       heart.style.width = "40px";
@@ -80,40 +72,7 @@
       moveHeart(heart)
     }
 
-    createHeart()
-
-// match heart and cupid = collied 
-
-function checkCollision(cupid, heart) {
-  let heartLeft = parseInt(heart.style.left)
-  let heartTop = parseInt(heart.style.top)
-  // let heartBottom = heartTop - 30
-  let cupidLeft = parseInt(cupid.style.left)
-  let cupidTop = parseInt(cupid.style.top)
-  if ((cupidLeft >= heartLeft -20 && cupidLeft <= heartLeft +20) &&
- (cupidTop >= heartTop -20 && cupidTop <=heartTop+20)) {
-    // console.log("heartbeat")
-   
-    return true 
-
-  }
-  return false
-  // if (cupidLeft === heartLeft && cupidTop === heartTop) {
-     
-}
-
-
-
-
-
-
-
-
-
-    
-    
     function moveHeart(heart) {
-
       let topNum = parseInt(heart.style.top);
       if (topNum > 500) {
           heart.remove();
@@ -127,17 +86,27 @@ function checkCollision(cupid, heart) {
       }
     }
 
-    // let heart = document.getElementById('heart')
-    moveHeart(heart)
+  // Checking for cupid and heart collision
+  function checkCollision(cupid, heart) {
+    let heartLeft = parseInt(heart.style.left)
+    let heartTop = parseInt(heart.style.top)
+    let cupidLeft = parseInt(cupid.style.left)
+    let cupidTop = parseInt(cupid.style.top)
 
-    // function heartDrop() {
-    //   createHeart()
-    //   moveHeart(heart)
-    // }
-    
-    function playGame() { 
+    if ((cupidLeft >= heartLeft -20 && cupidLeft <= heartLeft +20) &&
+      (cupidTop >= heartTop -20 && cupidTop <=heartTop+20)) {
+      return true
+      }
+    else if ((heartTop + 40 === 540))
+      if (window.confirm(`💔 Game over: You caught ${SCORE} hearts! Do you want to play again?`)) {
+        location.reload()
+      }
+    }
+
+
+    function playGame() {
       let counter = 0
-      heartInterval = setInterval(() => {  
+      heartInterval = setInterval(() => {
         let indexToRemove = -1
         FALLING_HEARTS.forEach(function(heart, index) {
           if (checkCollision(cupid, heart)) {
@@ -148,26 +117,29 @@ function checkCollision(cupid, heart) {
         })
         if (indexToRemove != -1){
           FALLING_HEARTS.splice(indexToRemove, 1)
-          
         }
-        if (counter == 21) { 
-          createHeart() 
+        if (counter == 21) {
+          createHeart()
           counter = 0
         }else {
           counter++
         }
         }, 100)
       }
- 
+
     playGame()
-    
-   
-    
+
+
+  // Incrementing score on each cupid/heart collision
   function incrementScore(){
     SCORE++
     let score = document.querySelector('#score')
     score.innerHTML = `
-      score: ${SCORE}
-     
+      SCORE: ${SCORE}
     `
   }
+
+  // function heartBreak(){
+  //   let heartbreak = document.querySelectorAll('heart')
+  //   heartbreak.src = 'images/heartbreak.png'
+  // }
